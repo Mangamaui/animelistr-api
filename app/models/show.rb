63d730ec0365a138dc0974a40ad7bf4842537c6b
Claudia Reynders <exotic.maui@gmail.com>
2017-11-03 12:33:03 +0100

    Added validation and relationships to models

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,4 @@
 class Show < ApplicationRecord
+
+  validates_presence_of :title, :episodes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   validates :password, :length => { :minimum => 6 }
+  validates :email
+
+  has_many :user_shows
+
 end
+
