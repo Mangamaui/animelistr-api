347294b557a4cf39e7b83345fccebef8f6683814
Claudia Reynders <exotic.maui@gmail.com>
2017-11-04 11:33:26 +0100

    Updated userShows migration with default data

--- db/migrate/20171102230103_create_user_shows.rb
+++ db/migrate/20171102230103_create_user_shows.rb
@@ -3,8 +3,8 @@ class CreateUserShows < ActiveRecord::Migration[5.1]
     create_table :user_shows do |t|
       t.references :user, index: true
       t.references :show, index: true
-      t.integer :status
-      t.integer :progress
+      t.integer :status, default: 1
+      t.integer :progress, default: 0
 
       t.timestamps
     end
