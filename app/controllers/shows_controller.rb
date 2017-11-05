bdfc02ccb56b896783b98820145916a75baeda04
Claudia Reynders <exotic.maui@gmail.com>
2017-11-05 23:31:34 +0100

    Created controllers for shows and userShows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,15 @@
 class ShowsController < ApplicationController
+
+  # GET /shows
+  def index
+    @shows = Show.all
+    render json: @shows
+  end
+
+  # GET /shows/:id
+  def show
+    @show = Show.find(params[:id])
+    render json: @show
+  end
+
 end
--- app/controllers/user_shows_controller.rb
+++ app/controllers/user_shows_controller.rb
@@ -1,2 +1,43 @@
 class UserShowsController < ApplicationController
+  before_action :set_userShow, :only => [:show, :update, :destroy]
+
+  # GET /usershows
+  def index
+    @user_shows = UserShow.where(user_id: params[:id])
+    render json: @user_shows
+  end
+
+  # GET /userShows/:id
+  def show
+    render json: @user_show
+  end
+
+  # POST /userShows
+  def create
+    @user_show = UserShow.new(params[:user_id], params[:show_id])
+    render json: @user_show
+  end
+
+  # DELETE /userShows/:id
+  def update
+    @user_show.update(userShow_params)
+    head :no_content
+  end
+
+  # PUT /userShows/:id
+  def destroy
+    @user_show.destroy
+    head :no_content
+  end
+
+
+  private
+  def userShow_params
+    params.permit(:status, :progress)
+  end
+
+  def set_userShow
+    @user_show = UserShow.find(params[:id])
+  end
+
 end
