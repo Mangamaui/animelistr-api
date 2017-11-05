96455c3f2dff047375ab711a3cb20d64b5d0a095
Claudia Reynders <exotic.maui@gmail.com>
2017-11-05 23:32:50 +0100

    Added db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+shows = Show.create([
+      {title: 'Shingeki no Kyojin', episodes: 25},
+      {title: 'Fullmetal Alchemist', episodes: 51},
+      {title: 'Steins Gate', episodes: 24},
+      {title: 'Barakamon', cover: '59321.jpg', episodes: 12},
+      {title: 'Hanasaku Iroha', cover: '28967.jpg', episodes: 26},
+      {title: 'Udon no Kuni no Kiniro Kemari', cover: '82343.jpg', episodes: 12},
+      {title: 'flying Witch', cover: '80039.jpg', episodes: 12},
+      {title: 'Natsume Yuujinchou', cover: '28859.jpg', episodes: 13}
+  ]
+
+)
