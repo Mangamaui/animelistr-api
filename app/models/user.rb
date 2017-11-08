711e4f563e323d5852cca33a1b0c017bfe4c02b8
Claudia Reynders <exotic.maui@gmail.com>
2017-11-08 10:54:17 +0100

    Updated validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :password, :length => { :minimum => 6 }
-  validates :email
+  validates :email, uniqueness: { case_sensitive: false }
 
   has_many :user_shows
-
 end
 
