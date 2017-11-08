805ba3df958e284792210d8d07b5dff7fc7e2f0b
Claudia Reynders <exotic.maui@gmail.com>
2017-11-08 10:42:11 +0100

    Updated User controller to work with Knock

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  #  POST /users
+  def create
+    @user = User.new(email: params[:email], password: params[:password])
+
+    if @user.save then
+      token = Knock::AuthToken.new payload: { sub: @user.id }
+      render json: token, status: :created
+    else
+      render json: { errors: @user.errors }, status: 422
+    end
+  end
+
 end
+
